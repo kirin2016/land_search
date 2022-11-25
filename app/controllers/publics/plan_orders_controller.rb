4dd5b06b4ef1e48b13d4ee16fa61d9ee82ebb4ca
kirin2016 <kiriyama.takuya.87r@gmail.com>
2022-11-25 18:16:22 +0000

    [Add]plan_order周り

--- app/controllers/publics/plan_orders_controller.rb
+++ app/controllers/publics/plan_orders_controller.rb
@@ -1,12 +1,66 @@
 class Publics::PlanOrdersController < ApplicationController
 
   def show
+    @plan_order = PlanOrder.find(params[:id])
   end
 
   def registration
+    @customer = Customer.find(current_customer.id)
+    @land_percel = LandPercel.find(params[:id])
+    @property = @land_percel.property
+    @plan_order = PlanOrder.new
   end
 
   def create
+    @plan_order = PlanOrder.new(plan_order_params)
+    @plan_order.customer_id = current_customer.id
+    if @plan_order.save
+      flash[:success] = "プラン作成依頼を受け付けました。"
+      redirect_to plan_order_path(@plan_order.id)
+    else
+      flash.now[:danger] = 'エラーが発生し、依頼を受付できませんでした。'
+      render registration
+    end
+  end
+
+  private
+
+  def plan_order_params
+    params.require(:plan_order).permit(
+      :land_percel_id,
+      :remarks,
+      :contact_datetime_1,
+      :contact_datetime_2,
+      :contact_datetime_3,
+      :tenant_number_order,
+      :tenant_member_order,
+      :house_type_order,
+      :total_budget_order,
+      :number_floor_order,
+      :floor_area_order,
+      :number_room_order,
+      :ldk_floor_number_order,
+      :ldk_area_order,
+      :kitchen_type_order,
+      :mbr_area_order,
+      :br_area_order,
+      :bathroom_area_order,
+      :japanese_room_order,
+      :storage_order,
+      :den_order,
+      :den_type_order,
+      :wic_order,
+      :family_closet_order,
+      :sic_order,
+      :pantry_order,
+      :loft_order,
+      :void_order,
+      :laundry_room_order,
+      :garden_order,
+      :parking_number_order,
+      :car_model_order,
+      :bicycle_parking_number_order,
+      :request_details_order)
   end
 
 end
